6b5f45dbb0cc1c73b7ff792642f61eafe4488fb6
Malomo282 <jesseappiah28@gmail.com>
2026-09-17 16:33:32 +0100

    Crop hero section height by 30%
    - Reduce hero section height from 90vh to 63vh
    - More compact hero banner while maintaining image quality

--- src/App.js
+++ src/App.js
@@ -309,7 +309,7 @@ function HeroSection() {
       <section style={{
         marginTop: 60,
         width: "100%",
-        height: "90vh",
+        height: "63vh",
         overflow: "hidden",
         background: "#000000",
       }}>
